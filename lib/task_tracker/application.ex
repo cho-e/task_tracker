d9410fc5b4b1db342a25eadca850effd801c943d
cho-e <cho.e@husky.neu.edu>
2019-03-01 11:23:53 -0500

    final changes

--- lib/task_tracker/application.ex
+++ lib/task_tracker/application.ex
@@ -1,19 +1,19 @@
 defmodule TaskTracker.Application do
-  use Application
-
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
-  def start(_type, _args) do
-    import Supervisor.Spec
+  @moduledoc false
 
-    # Define workers and child supervisors to be supervised
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
     children = [
       # Start the Ecto repository
-      supervisor(TaskTracker.Repo, []),
+      TaskTracker.Repo,
       # Start the endpoint when the application starts
-      supervisor(TaskTrackerWeb.Endpoint, []),
-      # Start your own worker by calling: TaskTracker.Worker.start_link(arg1, arg2, arg3)
-      # worker(TaskTracker.Worker, [arg1, arg2, arg3]),
+      TaskTrackerWeb.Endpoint
+      # Starts a worker by calling: TaskTracker.Worker.start_link(arg)
+      # {TaskTracker.Worker, arg},
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
