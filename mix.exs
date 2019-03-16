eec159b691d1b0f254b8061248f55ea14f739b54
cho-e <cho.e@husky.neu.edu>
2019-03-16 06:58:32 -0400

    Deploy

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TaskTracker.Mixfile do
   def project do
     [
       app: :task_tracker,
-      version: "0.0.1",
+      version: "0.1.0",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
